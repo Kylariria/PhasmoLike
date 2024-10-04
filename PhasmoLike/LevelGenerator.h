b9505fc934d4964c41c5f683fd4b6c3d51d1da88
UltiRed8 <84141770+UltiRed8@users.noreply.github.com>
2024-10-04 14:50:28 +0200

    Added LevelGenerator and GeneratorSettings

---
+++ PhasmoLike/LevelGenerator.h
@@ -0,0 +1,59 @@
+#pragma once
+
+struct GeneratorSettings
+{
+	// Rooms amounts
+	int kitchens = 0;
+	int garages = 0;
+	int bedrooms = 0;
+	int bathrooms = 0;
+	int corridors = 0;
+	int livingrooms = 0;
+	int globalAmount = 0;
+
+public:
+	bool IsValid() const { return globalAmount > 0; }
+
+public:
+	GeneratorSettings()
+	{
+		kitchens = 0;
+		garages = 0;
+		bedrooms = 0;
+		bathrooms = 0;
+		corridors = 0;
+		livingrooms = 0;
+		ComputeGlobalAmount();
+	}
+	GeneratorSettings(const int& _kitchens, const int& _garages, const int& _bedrooms, const int& _bathrooms, const int& _corridors, const int& _livingrooms)
+	{
+		kitchens = _kitchens;
+		garages = _garages;
+		bedrooms = _bedrooms;
+		bathrooms = _bathrooms;
+		corridors = _corridors;
+		livingrooms = _livingrooms;
+		ComputeGlobalAmount();
+	}
+
+private:
+	void ComputeGlobalAmount()
+	{
+		globalAmount = kitchens + garages + bedrooms + bathrooms + corridors + livingrooms;
+	}
+};
+
+class LevelGenerator
+{
+	GeneratorSettings settings;
+
+public:
+	LevelGenerator();
+	LevelGenerator(const GeneratorSettings& _settings);
+
+private:
+	bool CheckValidity();
+
+public:
+	void Generate();
+};
