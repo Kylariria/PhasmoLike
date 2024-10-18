7a56e7f8ef417ef3fa319a5bfec2f1320abd9c6d
Kylariria <iriapaquis.pro@gmail.com>
2024-10-18 15:32:18 +0200

    UI

---
+++ PhasmoLike/PathsMacro.h
@@ -0,0 +1,35 @@
+#pragma once
+
+//---------------- PATHS FOR ALL UIs ----------------
+
+//----------- FONT -----------
+
+#define PATH_TITLE_FONT "Ressources/Font/Title_Font.ttf" //Font typique Phasmo
+#define PATH_TEXT_FONT "Ressources/Font/Text_Font.ttf" //Font + douce // Menu
+
+//----------- BUTTONS PAUSE MENU -----------
+
+#define PATH_RESUMEBUTTON "Ressources/Images/UI/ButtonResume.png"
+#define PATH_OPTIONSBUTTON "Ressources/Images/UI/ButtonOptions.png"
+#define PATH_LEAVEBUTTON "Ressources/Images/UI/ButtonLeave.png"
+
+//----------- BUTTONS OPTIONS MENU -----------
+
+#define PATH_GAMEBUTTON "Ressources/Images/UI/ButtonGame.png"
+#define PATH_VIDEOBUTTON "Ressources/Images/UI/ButtonVideo.png"
+#define PATH_AUDIOBUTTON "Ressources/Images/UI/ButtonAudio.png"
+#define PATH_CONTROLSBUTTON "Ressources/Images/UI/ButtonControls.png"
+
+//----------- BUTTONS OPTIONS MENU -----------
+
+#define PATH_BACKGROUNDJOURNAL "Ressources/Images/UI/Background.png"
+
+//----------- SOUND -----------
+
+//TODO
+//if we want to implement some sound
+ 
+//----------- MUSIC -----------
+
+//TODO
+//if we want to implement some music
